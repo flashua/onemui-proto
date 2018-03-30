0269bbce0bb6b30fb435925fbfefa6960c3ed50b
Oleksandr Pastukhov <flashka.ua@gmail.com>
2018-03-30 12:55:14 +0200

    Update Todo.DataModel.js

--- src/components/Todo/Todo.DataModel.js
+++ src/components/Todo/Todo.DataModel.js
@@ -4,14 +4,14 @@ class TodoDataModel extends BaseDataModel{
   constructor(){
     super();
     this._data = {};
-    Registry.connect(this.reducer.bind(this), 'TodoDomainModel', 'OtherTodoDomainModel');
+    Registry.connect(this.selector.bind(this), 'TodoDomainModel', 'OtherTodoDomainModel');
   }
 
-  reducer(data) {
+  selector({TodoDomainModel, OtherTodoDomainModel}) {
     /**
      * all data from linked models goes here in Data arg
      */
-    this._data = {...data};
+    this._data = {TodoDomainModel, OtherTodoDomainModel};
     return data;
   }
 
